f17fa654b946c2beb16590eb706dc8d00110696f
sora-omaru <konggongxia317@gmail.com>
2026-08-11 09:12:17 +0900

    Initial commit

---
+++ src/main/java/com/omaru/keepsake_api/KeepsakeApiApplication.java
@@ -0,0 +1,13 @@
+package com.omaru.keepsake_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepsakeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepsakeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omaru/keepsake_api/KeepsakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omaru.keepsake_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepsakeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
