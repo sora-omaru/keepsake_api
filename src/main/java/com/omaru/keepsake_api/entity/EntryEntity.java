b720852e079fe4c9ac904cb953ce39f5dd2646ac
sora-omaru <konggongxia317@gmail.com>
2026-08-13 11:40:03 +0900

    feat:Entryentity作成

---
+++ src/main/java/com/omaru/keepsake_api/entity/EntryEntity.java
@@ -0,0 +1,59 @@
+package com.omaru.keepsake_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "entry")
+public class EntryEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id", nullable = false)
+    private WorkspaceEntity workspace;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(
+                    name = "workspace_id",
+                    referencedColumnName = "workspace_id",
+                    insertable = false,
+                    updatable = false
+            ),
+            @JoinColumn(
+                    name = "topic_id",
+                    referencedColumnName = "id"
+            )
+    })
+    private TopicEntity topic;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(
+                    name = "workspace_id",
+                    referencedColumnName = "workspace_id",
+                    insertable = false,
+                    updatable = false
+            ),
+            @JoinColumn(
+                    name = "member_id",
+                    referencedColumnName = "id"
+            )
+    })
+    private MemberEntity member;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column
+    private String content;
+
+
+}
