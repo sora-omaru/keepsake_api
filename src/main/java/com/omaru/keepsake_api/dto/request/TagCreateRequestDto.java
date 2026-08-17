a852d84b465c3a2396a042bffee5a7256a1d4815
sora-omaru <konggongxia317@gmail.com>
2026-08-17 13:51:58 +0900

    feat：空文字を入れたときのメッセージ作成

--- src/main/java/com/omaru/keepsake_api/dto/request/TagCreateRequestDto.java
+++ src/main/java/com/omaru/keepsake_api/dto/request/TagCreateRequestDto.java
@@ -3,5 +3,9 @@ package com.omaru.keepsake_api.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record TagCreateRequestDto(@NotBlank @Size(max = 100) String name) {
+public record TagCreateRequestDto(
+        @NotBlank(message = "タグの名前を決めてください")
+        @Size(max = 100)
+        String name
+) {
 }
