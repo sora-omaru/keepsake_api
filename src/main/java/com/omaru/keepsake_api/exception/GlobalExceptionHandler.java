391611e59c4aed4b1fb1b9d4c3bd2bacf8cdd818
sora-omaru <konggongxia317@gmail.com>
2026-08-17 13:53:15 +0900

    feat:Dtoのメッセージを返すように設定

--- src/main/java/com/omaru/keepsake_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/omaru/keepsake_api/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.omaru.keepsake_api.exception;
 
 import com.omaru.keepsake_api.dto.response.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +11,24 @@ import java.time.OffsetDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception
+    ) {
+        String message = exception.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(fieldError -> fieldError.getDefaultMessage())
+                .orElse("入力内容が不正です。");
+        ErrorResponseDto response = new ErrorResponseDto(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                message,
+                OffsetDateTime.now()
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponseDto> handleApiException(ApiException exception) {
