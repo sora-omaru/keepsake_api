b102fda554b9703fb21200c66bfe8b90f1b9c2bb
sora-omaru <konggongxia317@gmail.com>
2026-08-20 18:57:41 +0900

    feat:EntryTagRepositoryにてTagEntityを返すように追加

--- src/main/java/com/omaru/keepsake_api/repository/EntryTagRepository.java
+++ src/main/java/com/omaru/keepsake_api/repository/EntryTagRepository.java
@@ -2,7 +2,11 @@ package com.omaru.keepsake_api.repository;
 
 import com.omaru.keepsake_api.entity.EntryTagEntity;
 import com.omaru.keepsake_api.entity.EntryTagId;
+import com.omaru.keepsake_api.entity.TagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EntryTagRepository extends JpaRepository<EntryTagEntity, EntryTagId> {
     boolean existsByEntry_IdAndTag_Id(
@@ -11,4 +15,11 @@ public interface EntryTagRepository extends JpaRepository<EntryTagEntity, EntryT
     );
 
     void deleteByEntry_IdAndTag_Id(Long entryId, Long tagId);
+
+    @Query("""
+                SELECT et.tag
+                FROM EntryTagEntity et
+                WHERE et.entry.id = :entryId
+            """)
+    List<TagEntity> findTagsByEntryUd(Long entryId);
 }
