255debe73dc80d097f644810f4a3e109aec6e682
sora-omaru <konggongxia317@gmail.com>
2026-08-20 17:18:47 +0900

    feat:EntryRepositiryにfindByIdAndWorkspace_IdAndTopic_Id追加

--- src/main/java/com/omaru/keepsake_api/repository/EntryRepository.java
+++ src/main/java/com/omaru/keepsake_api/repository/EntryRepository.java
@@ -4,7 +4,10 @@ import com.omaru.keepsake_api.entity.EntryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EntryRepository extends JpaRepository<EntryEntity,Long> {
     List<EntryEntity> findByWorkspace_IdAndTopic_Id(Long workspaceId, Long topicId);
+
+    Optional<EntryEntity> findByIdAndWorkspace_IdAndTopic_Id(Long entryId, Long workspaceId, Long topicId);
 }
