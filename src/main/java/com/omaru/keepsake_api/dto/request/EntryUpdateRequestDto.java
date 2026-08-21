d7e2e5980fe96fa152149d15fa3e62af80e573f8
sora-omaru <konggongxia317@gmail.com>
2026-08-21 14:51:44 +0900

    feat:EntryUpdateRequestDtoに制約追加

--- src/main/java/com/omaru/keepsake_api/dto/request/EntryUpdateRequestDto.java
+++ src/main/java/com/omaru/keepsake_api/dto/request/EntryUpdateRequestDto.java
@@ -1,6 +1,11 @@
 package com.omaru.keepsake_api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record EntryUpdateRequestDto(
+        @NotBlank(message = "タイトルを入力してください")
+        @Size(max = 100, message = "タイトルは100文字以内で入力してください")
         String title,
         String content
 ) {
