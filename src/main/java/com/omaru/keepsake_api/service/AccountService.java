59d1fa7dcc61507b1f98dfe1e87e8759b4d47645
sora-omaru <konggongxia317@gmail.com>
2026-08-25 13:29:03 +0900

    feat:googleAccountService作成

--- src/main/java/com/omaru/keepsake_api/service/AccountService.java
+++ src/main/java/com/omaru/keepsake_api/service/AccountService.java
@@ -1,4 +1,8 @@
 package com.omaru.keepsake_api.service;
 
-public class AccountService {
+import com.omaru.keepsake_api.dto.account.GoogleAccountDto;
+import com.omaru.keepsake_api.entity.AccountEntity;
+
+public interface AccountService {
+AccountEntity findOrCreateByGoogle(GoogleAccountDto googleAccount);
 }
