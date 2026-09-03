48f883461894dbd58a8e6b5bdb49fbe01b4f7d0b
sora-omaru <konggongxia317@gmail.com>
2026-09-03 14:19:57 +0900

    feat:securityに自作フィルター適応。

--- src/main/java/com/omaru/keepsake_api/config/SecurityConfig.java
+++ src/main/java/com/omaru/keepsake_api/config/SecurityConfig.java
@@ -1,27 +1,32 @@
 package com.omaru.keepsake_api.config;
 
+import com.omaru.keepsake_api.security.JwtAuthenticationFilter;
 import com.omaru.keepsake_api.security.SuccessHandler;
+import com.omaru.keepsake_api.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final SuccessHandler successHandler;
+    private final JwtService jwtService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/", "/login/**", "/oauth2/**").permitAll()
                         .anyRequest().authenticated())
-                .oauth2Login(oauth2 -> oauth2.successHandler(successHandler));
+                .oauth2Login(oauth2 -> oauth2.successHandler(successHandler))
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(jwtService),
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
-
     }
-
 }
